5b0d3b7e41b09312fb410ccf26708cc6fdd2a8e6
Egor Krasovski <ekrasouski@icloud.com>
2019-09-12 14:54:16 +0300

    change java version and do some notes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
+        //todo some msg
         System.out.println("Ho-ho-ho");
         // more code
+        Stream.of();
     }
 }
